865622e5fba12b63e132f4f171e4c189d6b59fbc
dapzxxx <davaumarti@gmail.com>
2026-03-01 12:16:04 +0700

    🚀 Add HTML WebView Android project
    Generated by APKforge

--- app/src/main/java/com/dapz/forex/MainActivity.kt
+++ app/src/main/java/com/dapz/forex/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.statusBarColor = Color.parseColor("#6d28d9")
+            window.statusBarColor = Color.parseColor("#0000ff")
         }
 
         webView = WebView(this)
